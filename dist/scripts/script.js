4214bd065ae9f64942c65b0c80ea42c3cd312621
Anna Burdanova <burdanovaas@gmail.com>
2024-02-15 17:23:05 +0700

    Add responsive navigation menu

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -17,3 +17,27 @@ const togglePopup = () => {
   document.body.classList.toggle("popup-active");
   popupElement.classList.toggle("active");
 };
+
+// Handle navigation menu opening and closing
+const navMenuBtn = document.querySelector("#nav-btn");
+const navMenu = document.querySelector("#nav-menu");
+
+let navMenuOpen = false;
+const toggleNavMenu = () => {
+  document.body.classList.toggle("nav-active");
+  navMenu.classList.toggle("active");
+  navMenuOpen = !navMenuOpen;
+};
+
+navMenuBtn.addEventListener("click", toggleNavMenu);
+
+const target = document.querySelector(".header__nav");
+const handleOutsideClick = (e) => {
+  const withinBoundaries = e.composedPath().includes(target);
+
+  if (navMenuOpen && !withinBoundaries) {
+    toggleNavMenu();
+  }
+};
+
+document.addEventListener("click", handleOutsideClick);
